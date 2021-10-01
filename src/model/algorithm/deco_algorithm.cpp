4190242a3bb78b78549ba237da1f30c2f7cdaca3
Robert Kerr <robert_kerr@blueyonder.co.uk>
2021-10-01 22:07:03 +0100

    Make deco-seg conversions in stack

--- src/model/algorithm/deco_algorithm.cpp
+++ src/model/algorithm/deco_algorithm.cpp
@@ -11,67 +11,63 @@ using Tissues = DecoModel::Tissues;
 //******************************************
 // Deco stops algorithm
 //******************************************
-std::vector<DecoStop*> get_deco_stops(Tissues compartments) {
+std::vector<DecoStop> get_deco_stops(Tissues compartments) {
     //Note that compartments and all the properties of it are in the stack
     // so the object is essentially copied
     float time;
     int num_stops, stop_depth;
-    std::vector<DecoStop*> deco_stops;
+    std::vector<DecoStop> deco_stops;
 
-    
 
+
+
+
+    return deco_stops;
 }
 
 
 //******************************************
 // Turn a single deco stop into a segment
 //******************************************
-static Segment* stop2seg(DecoStop* stop) {
-    Segment* segment;
-    segment = new Segment(stop->depth, 0, stop->time, stop->gas);
-    return segment;
+static Segment stop2seg(DecoStop* stop) {
+    return Segment(stop->depth, 0, stop->time, stop->gas);
 }
 
 
 //******************************************
 // Create ascent segment
 //******************************************
-static Segment* asc_seg(int depth1, int depth2, std::vector<int> gas, bool in_deco) {
+static Segment asc_seg(int depth1, int depth2, std::vector<int> gas, bool in_deco) {
     int rate, time;
-    Segment* segment;
 
     //Pick ascent rate
     rate = in_deco == true ? MAX_DECO_ASCENT_RATE : MAX_ASCENT_RATE;
 
     //Find time and create segment
     time = static_cast<int> ((depth2 - depth1) / rate);
-    segment = new Segment(depth1, rate, time, gas);
-    return segment;
+    return Segment(depth1, rate, time, gas);
 }
 
 
 //******************************************
 // Turn deco stops to dive segments
 //******************************************
-std::vector<Segment*> DecoModel::deco2seg(int bottom_depth,
+std::vector<Segment> DecoModel::deco2seg(int bottom_depth,
     std::vector<DecoStop*> stops, std::vector<int> bottom_gas) {
 
     int depth, next_depth, stop_num;
-    std::vector<Segment*> segments;
-    Segment* segment;
+    std::vector<Segment> segments;
     DecoStop* stop;
 
     next_depth = stops[0]->depth; 
 
     //Ascent from bottom to first stop
-    segment = asc_seg(next_depth, bottom_depth, bottom_gas, false);
-    segments.push_back(segment);
+    segments.push_back(asc_seg(next_depth, bottom_depth, bottom_gas, false));
 
     //Deco stops and ascents
     for (stop_num = 0; stop_num < stops.size(); stop_num++) {
         stop = stops[stop_num];
-        segment = stop2seg(stop);
-        segments.push_back(segment);
+        segments.push_back(stop2seg(stop));
 
         depth = stop->depth;
 
@@ -80,8 +76,7 @@ std::vector<Segment*> DecoModel::deco2seg(int bottom_depth,
                         : next_depth = stops[stop_num + 1]->depth;
 
         //Append ascent to next stop
-        segment = asc_seg(depth, next_depth, stop->gas, true);
-        segments.push_back(segment);
+        segments.push_back(asc_seg(depth, next_depth, stop->gas, true));
     } 
 
     return segments;
